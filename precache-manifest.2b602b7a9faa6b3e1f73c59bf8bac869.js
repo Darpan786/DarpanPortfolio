53c5c1069d326219b1602c0ad33fe76d8d1b12fa
Darpan <darpanagarwal1999@gmail.com>
2020-10-19 23:56:57 +0530

    Updates

---
+++ precache-manifest.2b602b7a9faa6b3e1f73c59bf8bac869.js
@@ -0,0 +1,186 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7afd77ce9bca088220b16e687c7a2f73",
+    "url": "/DarpanPortfolio/index.html"
+  },
+  {
+    "revision": "fa24c604da4f2bedfdab",
+    "url": "/DarpanPortfolio/static/css/main.ff2d0439.chunk.css"
+  },
+  {
+    "revision": "dc381f0233c846718284",
+    "url": "/DarpanPortfolio/static/js/2.8328f34b.chunk.js"
+  },
+  {
+    "revision": "176c1a1a28b19c611d9701905d8d60b7",
+    "url": "/DarpanPortfolio/static/js/2.8328f34b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa24c604da4f2bedfdab",
+    "url": "/DarpanPortfolio/static/js/main.1c59e7a5.chunk.js"
+  },
+  {
+    "revision": "81ce70b0710883202c3d",
+    "url": "/DarpanPortfolio/static/js/runtime-main.7119e948.js"
+  },
+  {
+    "revision": "f6491bfa4afa66fbfbffac5ee4b507e2",
+    "url": "/DarpanPortfolio/static/media/AkanshaGYM.f6491bfa.webp"
+  },
+  {
+    "revision": "b0109c90f43a90ee2ab9039a1e228735",
+    "url": "/DarpanPortfolio/static/media/React_todo.b0109c90.webp"
+  },
+  {
+    "revision": "bf989778565c49da91f9e0ef13dbdc5c",
+    "url": "/DarpanPortfolio/static/media/background.bf989778.webp"
+  },
+  {
+    "revision": "e95d05630894e5403e569fdaa0a2b61a",
+    "url": "/DarpanPortfolio/static/media/background.e95d0563.webp"
+  },
+  {
+    "revision": "ffded2381d25dd906406d6e07f91dea5",
+    "url": "/DarpanPortfolio/static/media/bootstrap-4.ffded238.svg"
+  },
+  {
+    "revision": "e47abf353b82b9bef0570369e92a62fb",
+    "url": "/DarpanPortfolio/static/media/corona.e47abf35.webp"
+  },
+  {
+    "revision": "3707da3c6a394c7ee2523273f8d98a25",
+    "url": "/DarpanPortfolio/static/media/css3.3707da3c.svg"
+  },
+  {
+    "revision": "add239172d50d6ad18d2ac3cea6257a1",
+    "url": "/DarpanPortfolio/static/media/digital-ocean.add23917.svg"
+  },
+  {
+    "revision": "bcf22073c9f13bab1761b90bce9fec55",
+    "url": "/DarpanPortfolio/static/media/django.bcf22073.svg"
+  },
+  {
+    "revision": "c6bab64b183f96aa47077fd3db37afd0",
+    "url": "/DarpanPortfolio/static/media/express.c6bab64b.svg"
+  },
+  {
+    "revision": "10b0719343408095f5a9b369e0787115",
+    "url": "/DarpanPortfolio/static/media/firebase.10b07193.svg"
+  },
+  {
+    "revision": "d98463abd102acdc2b1eb8d5eb70b2d2",
+    "url": "/DarpanPortfolio/static/media/flask.d98463ab.svg"
+  },
+  {
+    "revision": "1190a19ce3cc940741a60137a1e074cb",
+    "url": "/DarpanPortfolio/static/media/git-icon.1190a19c.svg"
+  },
+  {
+    "revision": "0555bfb7e81f57a1efe6a53c7490abb7",
+    "url": "/DarpanPortfolio/static/media/github-api.0555bfb7.svg"
+  },
+  {
+    "revision": "0ff1a6f08182be4aa63a0b54da1b9e57",
+    "url": "/DarpanPortfolio/static/media/github.0ff1a6f0.svg"
+  },
+  {
+    "revision": "30bd42ea97a2e0454bb1dfbab983eb54",
+    "url": "/DarpanPortfolio/static/media/go-6.30bd42ea.svg"
+  },
+  {
+    "revision": "b38ff61aa327fbdc24f7b1dad7d04d1a",
+    "url": "/DarpanPortfolio/static/media/heroku.b38ff61a.svg"
+  },
+  {
+    "revision": "676e8188f003ed5d4882a0d0f378dad3",
+    "url": "/DarpanPortfolio/static/media/html-5.676e8188.svg"
+  },
+  {
+    "revision": "2d2e31cc63a96c7e6044f52d30fd21b9",
+    "url": "/DarpanPortfolio/static/media/javascript.2d2e31cc.svg"
+  },
+  {
+    "revision": "82291019f34ab54a677c456a84127cc3",
+    "url": "/DarpanPortfolio/static/media/material-ui-1.82291019.svg"
+  },
+  {
+    "revision": "3bd7eec39a60581df5a9af247f02c9b0",
+    "url": "/DarpanPortfolio/static/media/messenger.3bd7eec3.webp"
+  },
+  {
+    "revision": "2985235df31b8742d893d5b7e80145c5",
+    "url": "/DarpanPortfolio/static/media/mongodb.2985235d.svg"
+  },
+  {
+    "revision": "54d7bddcd3019ec3c3b23db00160ccef",
+    "url": "/DarpanPortfolio/static/media/mssql.54d7bddc.svg"
+  },
+  {
+    "revision": "b198d1411862cf0e5fa74070ea6d2a13",
+    "url": "/DarpanPortfolio/static/media/node_todo.b198d141.webp"
+  },
+  {
+    "revision": "a123152868245dee71c441124a98ac3e",
+    "url": "/DarpanPortfolio/static/media/nodejs.a1231528.svg"
+  },
+  {
+    "revision": "d7c985e7cca077dc2d77f89a66573e35",
+    "url": "/DarpanPortfolio/static/media/postgresql.d7c985e7.svg"
+  },
+  {
+    "revision": "00a779962ffd5307b19fa711f12be661",
+    "url": "/DarpanPortfolio/static/media/profile.00a77996.webp"
+  },
+  {
+    "revision": "146716bdf1a351db5705ec1e6a25ed08",
+    "url": "/DarpanPortfolio/static/media/python.146716bd.svg"
+  },
+  {
+    "revision": "83fe61b05935f090b671b08aeb77b63d",
+    "url": "/DarpanPortfolio/static/media/react-bootstrap.83fe61b0.svg"
+  },
+  {
+    "revision": "024196716802e272adb3cbefb81fd557",
+    "url": "/DarpanPortfolio/static/media/react-router.02419671.svg"
+  },
+  {
+    "revision": "4009eef3b3e291f296e71962c70895c5",
+    "url": "/DarpanPortfolio/static/media/react.4009eef3.svg"
+  },
+  {
+    "revision": "aa0a337603611296524a666b62a9ff23",
+    "url": "/DarpanPortfolio/static/media/redux.aa0a3376.svg"
+  },
+  {
+    "revision": "b446ca5a397c1ef6fb94cd02c4e94335",
+    "url": "/DarpanPortfolio/static/media/sass-1.b446ca5a.svg"
+  },
+  {
+    "revision": "8bce7aef886c3872c218e0e6ef8bdccb",
+    "url": "/DarpanPortfolio/static/media/slide1.8bce7aef.webp"
+  },
+  {
+    "revision": "7256f309520276be70acd97720610bc6",
+    "url": "/DarpanPortfolio/static/media/slide2.7256f309.webp"
+  },
+  {
+    "revision": "8e5b87b60151049a8df9ec0bd30978f0",
+    "url": "/DarpanPortfolio/static/media/slide3.8e5b87b6.webp"
+  },
+  {
+    "revision": "f735b382642369168ff2de8b2d1786be",
+    "url": "/DarpanPortfolio/static/media/srgi.f735b382.webp"
+  },
+  {
+    "revision": "e79a42b9281a3b30f1302d5452f9884f",
+    "url": "/DarpanPortfolio/static/media/styled-components.e79a42b9.svg"
+  },
+  {
+    "revision": "3de182d2120823fe49c27eeb1765c53a",
+    "url": "/DarpanPortfolio/static/media/typescript.3de182d2.svg"
+  },
+  {
+    "revision": "ded5e2b1ad1371b1168b165aac116ad3",
+    "url": "/DarpanPortfolio/static/media/writerdesk.ded5e2b1.webp"
+  }
+]);
